23426f1a8ae2809006275e83305631452f15277c
kss418 <108602943+kss418@users.noreply.github.com>
2022-09-15 14:24:11 +0900

    Add files via upload

---
+++ hw2.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+double dist;
+
+int main() {
+	printf("Please enter kilometers: ");
+	scanf("%f", &dist);
+	printf("%.1f km is equal to %.1f miles", dist, dist / 1.609);
+
+	return 0;
+}
