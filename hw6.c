c35e5309f4801fb45f1b63ae334d415226827a98
kss418 <108602943+kss418@users.noreply.github.com>
2022-10-31 20:11:31 +0900

    Add files via upload

---
+++ hw6.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main() {
+    printf("Please input five integers: ");
+    int num;
+    int odd[5], even[5];
+    int odd_cnt = 0, even_cnt = 0;
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &num);
+        if (num % 2) {
+            odd[odd_cnt] = num;
+            odd_cnt++;
+        }
+        else {
+            even[even_cnt] = num;
+            even_cnt++;
+        }
+    }
+
+    printf("Odd numbers: ");
+    for (int i = 0; i < odd_cnt; i++) {
+        printf("%d ", odd[i]);
+    }
+    printf("\n");
+
+    printf("Even numbers: ");
+    for (int i = 0; i < even_cnt; i++) {
+        printf("%d ", even[i]);
+    }
+    printf("\n");
+
+    
+
+    
+}
+
