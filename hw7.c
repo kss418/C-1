757b6dcead28fd4cb94857189943ecfe62464b37
kss418 <108602943+kss418@users.noreply.github.com>
2022-11-07 10:40:28 +0900

    Add files via upload

---
+++ hw7.c
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void print(int* arr) {
+	for (int i = 0; i < 6; i++) {
+		printf("%d ", *(arr + i));
+	}
+	printf("\n");
+	return;
+}
+
+
+int main() {
+	int arr1[6] = { 1,2,3,4,5,6 }, arr2[6] = { 7,8,9,10,11,12 };
+	int* ptr1 = arr1;
+	int* ptr2 = arr2;
+
+	printf("arr1: ");
+	print(ptr1);
+	printf("arr2: ");
+	print(ptr2);
+	printf("\n");
+
+	int* temp = ptr1;
+	ptr1 = ptr2;
+	ptr2 = temp;
+
+	printf("after swap\n");
+	printf("arr1: ");
+	print(ptr1);
+	printf("arr2: ");
+	print(ptr2);
+
+
+	return 0;
+}
