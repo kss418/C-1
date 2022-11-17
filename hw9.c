1b272a3841c49b8ade98eef0aa6e9671ef6ae273
kss418 <108602943+kss418@users.noreply.github.com>
2022-11-17 12:40:11 +0900

    Add files via upload

---
+++ hw9.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+
+
+int main() {
+	char string[100];
+	fgets(string, sizeof(string), stdin);
+	
+	for (int i = 0; i < strlen(string); i++) {
+		if (string[i] >= 97 && string[i] <= 122) {
+			printf("%c", string[i] - 32);
+		}
+		else if (string[i] >= 65 && string[i] <= 90) {
+			printf("%c", string[i] + 32);
+		}
+		else {
+			printf("%c", string[i]);
+		}
+	}
+
+
+
+
+
+	return 0;
+}
